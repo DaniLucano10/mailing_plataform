e5371de31c8e9915001ee4d211ac719ad464d4cd
DaniLucano10 <olucano10@gmail.com>
2025-01-29 15:52:20 -0500

    buscar usuario or email

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -25,6 +25,11 @@ export class UsersService {
 
     return this.usersRepository.save(newUser);
   }
+
+  // Método para buscar usuario por email
+  async findByEmail(email: string): Promise<User | null> {
+    return await this.usersRepository.findOne({ where: { email } });
+  }
   async findAll(): Promise<User[]> {
     return this.usersRepository.find();
   }
