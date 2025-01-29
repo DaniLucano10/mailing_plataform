76f2e8aa73c19b2218ddf48bde7451ecc8d0e351
DaniLucano10 <olucano10@gmail.com>
2025-01-29 15:49:09 -0500

    importaciones

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthGuard } from './guard/auth.guard';
+
+@Module({
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '60m' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, AuthGuard],
+  exports: [JwtModule],
+})
+export class AuthModule {}
