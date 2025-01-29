029e6c3110b22483cf04a1bdb67618b54c62a3ce
DaniLucano10 <olucano10@gmail.com>
2025-01-29 17:39:07 -0500

    entidades de usuarios

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,21 +1,35 @@
-import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { List } from 'src/lists/entities/list.entity';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 @Entity('users')
 export class User {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({ unique: true })
-    username: string;
+  @Column({ unique: true })
+  username: string;
 
-    @Column({ unique: true, nullable: false })
-    email: string;
+  @Column({ unique: true, nullable: false })
+  email: string;
 
-    @Column({ nullable: false })
-    password: string;
+  @Column({ nullable: false })
+  password: string;
 
-    @CreateDateColumn({ type: 'timestamp'})
-    created_at: Date;
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
 
-    @UpdateDateColumn({ type: 'timestamp'})
-    updated_at: Date;
+  @UpdateDateColumn({ type: 'timestamp' })
+  updated_at: Date;
+
+  @OneToMany(() => List, (list) => list.user) // Relación de uno a muchos
+  @JoinColumn({ name: 'user_id' })
+  lists: List[];
 }
