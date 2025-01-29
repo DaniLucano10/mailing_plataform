e9d78151ed7a6be31542a569a2177d7e1deec269
DaniLucano10 <olucano10@gmail.com>
2025-01-29 15:48:17 -0500

    configuracion para swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
+dotenv.config(); 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,7 +17,20 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('API Docs')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   const port = process.env.NEST_PORT || 3000;
   await app.listen(port);
+
+  
 }
 bootstrap();
