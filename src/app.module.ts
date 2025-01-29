1940fba9705934d1b7d8043b056ef9c4d2c3b405
DaniLucano10 <olucano10@gmail.com>
2025-01-28 20:02:55 -0500

    realizando cambios

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',  // Cambia esto si tu base de datos está en otro servidor
+      port: 5432,         // Puerto de PostgreSQL
+      username: 'postgres', // Tu usuario de PostgreSQL
+      password: '123456',  // Tu contraseña de PostgreSQL
+      database: 'mailing_platform',  // Nombre de la base de datos
+      entities: [User], // Asegúrate de que las entidades estén en el directorio correcto
+      synchronize: true,  // Solo en desarrollo, en producción usar migraciones
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,18 @@
-export class User {}
+import { Column, DeleteDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    username: string;
+
+    @Column({ unique: true, nullable: false })
+    email: string;
+
+    @Column({ nullable: false })
+    password: string;
+
+    @DeleteDateColumn()
+    deletedAt: Date;
+}
