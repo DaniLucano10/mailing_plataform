4d02da14476940279e07be39a3d9644fdeaa8745
DaniLucano10 <olucano10@gmail.com>
2025-01-29 17:38:15 -0500

    Agregando listas aal modulo de user

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,11 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { List } from 'src/lists/entities/list.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forFeature([User, List]),
   ],
   controllers: [UsersController],
   providers: [UsersService],
