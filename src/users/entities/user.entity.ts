9ac6bb14bb139c814fd1d3b23859a147c25bd252
DaniLucano10 <olucano10@gmail.com>
2025-01-31 22:47:57 -0500

    cambios

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -15,7 +15,7 @@ export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ unique: true })
+  @Column({ unique: true, nullable: false })
   username: string;
 
   @Column({ unique: true, nullable: false })
